ba6441deaa11f0fdc7be959c5eed76e10e2e88ee
Nick <nponikie@umich.edu>
2018-05-15 18:49:14 -0400

    updated AI

--- SuperHeroForHireV2/Assets/Scripts/EnemyAI.cs
+++ SuperHeroForHireV2/Assets/Scripts/EnemyAI.cs
@@ -14,12 +14,15 @@ public class EnemyAI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(XMoveDirection, 0));
+        RaycastHit2D hole = Physics2D.Raycast(new Vector2(transform.position.x+5,transform.position.y), new Vector2(0, 1));
+        
+
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XMoveDirection, 0) * EnemySpeed;
-        if(hit.distance < 1.1f)
+        if ((hit.distance < 1.1f) && (hit.distance != 0))
         {
             Flip();
         }
-	}
+    }
     void Flip()
     {
         if(XMoveDirection > 0)
