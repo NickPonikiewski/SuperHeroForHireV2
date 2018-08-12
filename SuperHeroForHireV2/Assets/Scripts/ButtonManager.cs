6dffb5fdc5ee1a41419856b6e50163d115c6856a
hamadehson <hamadehson@gmail.com>
2018-08-12 12:10:46 -0400

    Added Prefabs

---
+++ SuperHeroForHireV2/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    IEnumerator Wait(float sec, string newGameLevel)
+    {
+        yield return new WaitForSeconds(sec);
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+	public void NewGameButton (string newGameLevel)
+    {
+        StartCoroutine(Wait(1, newGameLevel));
+       
+    }
+	
+    public void StoryPage (string Story)
+    {
+        SceneManager.LoadScene(Story);
+    }
+
+    public void HowtoplayButton (string Instructions)
+    {
+        SceneManager.LoadScene(Instructions);
+    }
+
+    public void Return (string Menu)
+    {
+        SceneManager.LoadScene(Menu);
+    }
+
+    public void ExitGameBtn()
+    {
+        Application.Quit();
+    }
+
+}
