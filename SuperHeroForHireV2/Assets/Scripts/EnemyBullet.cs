ec387799261faf5adc6ac52d8d90335edb8fbd5e
Nick <nponikie@umich.edu>
2018-08-18 17:07:27 -0400

    AI

---
+++ SuperHeroForHireV2/Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour {
+
+    private Transform target;
+
+    public float speed = 70f;
+
+    Vector3 OldDir;
+
+    public void Seek (Transform _target) //can be used for spawn effect, set speed, dam amount
+    {
+        target = _target;
+    }
+    private void Start()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+        OldDir = dir;
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+    }
+    // Update is called once per frame
+    void Update () {
+		if(target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if(dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(OldDir.normalized * distanceThisFrame, Space.World);
+	}
+
+    void HitTarget()
+    {
+        Destroy(gameObject);
+    }
+}
