462a397c28485b4273cd29fbe46d660a261d2f9a
hamadehson <hamadehson@gmail.com>
2018-08-19 10:24:37 -0400

    healthbar shiiiiieeeett

--- SuperHeroForHireV2/Assets/Scripts/PlayerScript.cs
+++ SuperHeroForHireV2/Assets/Scripts/PlayerScript.cs
@@ -3,91 +3,32 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
-    private Transform pBody;
+    [SerializeField]
+    private Stat health;
 
-    private Rigidbody2D pRigid;
-
-    public bool _Direction;
-
-    public ArmRotation _Arm;
-
-    private bool _Grounded;
-
-    [SerializeField]  private LayerMask whatIsGround;
-
-    [SerializeField]  private Transform[] groundPoints;
-
-    private bool _Jump;
-
-    public float jumpHight;
-
-    public float _Speed;
-
-    public float _Horizontal;
-
-    void Awake ()
-
+    private void Awake()
     {
-        pBody = transform.Find("Body");
-
-        pRigid = gameObject.GetComponent<Rigidbody2D>();
-
-        _Direction = true;
-
+        health.Initialize();
     }
 
-	void Start ()
-
-    {
-	
-        
+    void Start ()
+    { 
 
 	}
 	
-
-    void FixedUpdate ()
-
-    {
-
-        float _Horizontal = Input.GetAxis("Horizontal");
-
-        float _Vertical = Input.GetAxis("Vertical");
-
-        Movements(_Horizontal);
-
-      
-    }
-
 	void Update ()
-
     {
-	
-	}
-
-    void Movements (float _Horizontal)
-
-    {
-
-            pRigid.AddForce(new Vector2(_Speed*_Horizontal, pRigid.velocity.y));
-
+       
+        //changes the players health
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            health.CurrentVal -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            health.CurrentVal += 10;
+        }
 
     }
-
-  
-
-    public void Flipp ()
-
-    {
-
-        
-            _Direction = !_Direction;
-
-            Vector3 theScale = pBody.localScale;
-
-            theScale.x *= -1f;
-
-            pBody.localScale = theScale;
-        
-
-    }
+    
 }
---
+++ SuperHeroForHireV2/Assets/Scripts/Stat.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class Stat
+{
+    [SerializeField]
+    private BarScript bar;
+
+    [SerializeField]
+    private float maxVal;
+
+    [SerializeField]
+    private float currentVal;
+
+    public float CurrentVal
+    {
+        get
+        {
+            return currentVal;
+        }
+
+        set
+        {  
+            this.currentVal = value;
+            bar.Value = currentVal;
+        }
+    }
+
+    public float MaxVal
+    {
+        get
+        {
+            return maxVal;
+        }
+
+        set
+        {
+            bar.MaxValue = value;
+            this.maxVal = value;
+        }
+    }
+
+
+    public void Initialize()
+    {
+        this.MaxVal = maxVal;
+        this.CurrentVal = currentVal;
+    }
+}
