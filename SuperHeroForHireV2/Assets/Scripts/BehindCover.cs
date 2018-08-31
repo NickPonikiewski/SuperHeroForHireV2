3628f5f732c8437d88b35d683f98bbe8c7184363
Nick <nponikie@umich.edu>
2018-08-30 20:10:11 -0400

    no more sticky walls and started cover system
    p.s dan is a Lord of the house Gay

---
+++ SuperHeroForHireV2/Assets/Scripts/BehindCover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehindCover : MonoBehaviour {
+
+    public Transform Player;
+    public float MinPlayerDistance;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float PlayerDis = Vector3.Distance(Player.position, transform.position);
+
+		if(Input.GetKeyDown(KeyCode.E) && PlayerDis <= MinPlayerDistance)
+        {
+            //move player to edge of surface stop movement (maybe just a sprite and hide real player)
+            Debug.Log("Player in cover");
+
+        }
+	}
+}
